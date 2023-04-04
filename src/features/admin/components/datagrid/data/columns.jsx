780f897e74491dd5fc5dec194134f10bce281428
Kangonga <guchu413@gmail.com>
2023-04-04 11:27:03 +0300

    feat: remove hardcoded data in the admin data table

--- src/features/admin/components/datagrid/data/columns.jsx
+++ src/features/admin/components/datagrid/data/columns.jsx
@@ -1,29 +1,10 @@
-export const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-];
+import { Avatar } from "@mui/material";
 
 export const devColumns = [
-    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'id', headerName: 'ID', width: 150 },
     { field: 'firstName', headerName: 'First name', width: 130 },
     { field: 'lastName', headerName: 'Last name', width: 130 },
-    { field: 'closed', headerName: 'Total closed', width: 130 },
+    { field: 'closedTickets', headerName: 'Total closed', width: 130 },
     { field: 'status', headerName: 'Status', width: 130, renderCell:(params)=>params.row.status },
     {
       field: 'avatar',
@@ -31,7 +12,7 @@ export const devColumns = [
       width: 90,
       renderCell:(params)=>{
         return(<>
-            {params.row.avatar}
+        <Avatar src={`${params.row.avatar}`}></Avatar>
         </>)
       }
     }
@@ -52,27 +33,4 @@ export const agentColumns = [
         </>)
       }
     }
-];
-
-export const ticketColumns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'title', headerName: 'title', width: 200 },
-    { field: 'agent', headerName: 'Reported by', width: 130 },
-    { field: 'status', headerName: 'Status', width: 130 },
-    { field: 'assignee', headerName: 'Assignee', width: 130},
-    { field: 'source', headerName: 'Source', width: 130 },
-    { field: 'priority', headerName: 'Priority', width: 130 },
-];
-
-export const closedTicketColumns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'title', headerName: 'title', width: 200 },
-    { field: 'agent', headerName: 'Reported by', width: 130 },
-    { field: 'status', headerName: 'Status', width: 130 },
-    { field: 'assignee', headerName: 'Assignee', width: 130},
-    { field: 'source', headerName: 'Source', width: 130 },
-    { field: 'priority', headerName: 'Priority', width: 130 },
-    { field: 'open', headerName: 'Opened at', width: 130 },
-    { field: 'close', headerName: 'Closed at', width: 130 },
-
 ];
