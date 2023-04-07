90fa190b2825272ce4def4c65eac915ffcee03ab
Kangonga <guchu413@gmail.com>
2023-04-07 06:38:41 +0300

    style: style the chats component in the ticketChat page

--- src/shared/pages/ticketChat/styles.jsx
+++ src/shared/pages/ticketChat/styles.jsx
@@ -3,25 +3,45 @@ import { styled } from "@mui/material";
 export const ComponentContainer = styled('div')(({ theme })=>({
     flex:2,
     display:'flex',
-    flexDirection:'column'
+    flexDirection:'column',
 }))
 
 export const ChatContainer = styled('div')(({ theme })=>({
     backgroundColor:'#fcfcfc',
     display:'flex',
-    flexDirection:'column'
+    flexDirection:'column',
 }))
 
 export const ChatIntro = styled('div')(({ theme })=>({
     display:'flex',
-    flexDirection:'column'
+    paddingRight:'1rem',
+    justifyContent:'space-between',
+    // gap:'2rem',
+    alignItems:'center',
+    [theme.breakpoints.down('sm')]:{
+        flexDirection:'column'
+    }
 }))
 
-export const Chats = styled('div')(({ theme })=>({
+export const ChatMessages = styled('div')(({ theme })=>({
     display:'flex',
     flexDirection:'column',
     height:'50vh',
-    overflow:'auto'
+    overflow:'auto',
+    gap:'1rem',
+    padding:'1rem',
+    marginBottom:'2rem',
+    width:'90%',    
+    '::-webkit-scrollbar': {
+        width: '10px'
+    },
+    '&::-webkit-scrollbar-track': {
+    background: '#f1f1f1',
+    },
+    '&::-webkit-scrollbar-thumb': {
+    background: '#888',
+    borderRadius: '4px',
+    },
 }))
 
 export const MessageCard = styled('div')(({ theme })=>({
@@ -29,12 +49,11 @@ export const MessageCard = styled('div')(({ theme })=>({
     flexDirection:'column',
     borderRadius: ' 10px',
     boxShadow: ' 2px 2px 5px rgba(0, 0, 0, 0.3)',
-    padding: ' 20px',
-    width:'60rem',
+    padding: '1rem',
+    width:'50rem',
 }))
 
 export const CreateMessageBox = styled('textarea')(({ theme })=>({
-    border:'2px solid black',
     height:'28vh',
     width:'60rem',
     overflow:'auto',
