30776cd3737735402388daa475a85541db828ede
Kangonga <guchu413@gmail.com>
2023-04-07 06:37:47 +0300

    refactor: move and rename the postTicket function

--- src/data/postData.jsx
+++ src/data/postData.jsx
@@ -9,17 +9,8 @@ export const postTickets = async (formdata)=>{
     const data = await res.json()
     return data
 }
-export const postTicket= async (formdata,id)=>{
-    const res = await fetch(`http://localhost:5000/tickets/${id}`, {
-        method:'PATCH',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(formdata)
-    })
-    const data = await res.json()
-    return data
-}
+
+
 
 export const postDeveloper = async (formdata)=>{
     const res = await fetch('http://localhost:5000/developers', {
