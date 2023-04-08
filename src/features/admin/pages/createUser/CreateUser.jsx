a14152dc2d49fc3e09e7747b8dfdd52cdfd1d987
Kangonga <guchu413@gmail.com>
2023-04-08 19:28:30 +0300

    feat: refactor the form to avoid manually typing out components:     Add reset form functionality

--- src/features/admin/pages/createUser/CreateUser.jsx
+++ src/features/admin/pages/createUser/CreateUser.jsx
@@ -8,10 +8,11 @@ import { Container } from '../../../../shared/styles/styles'
 import Sidebar from '../../../../shared/components/sidebar/Sidebar'
 import Topbar from '../../../../shared/components/topbar/Topbar'
 import { postAgents, postDeveloper } from '../../../../data/postData'
+import { useState } from 'react'
 
 export default function CreateUser({type}) {
   const date = new Date()
-    const initialValues = {
+    const [initialValues, setInitialValues] = useState({
         id:type==='developer'?new IdGenerator(`dev${date.getFullYear()}{{ string_2 }}{{ number_2 }}`).getFinalExpression():new IdGenerator(`agent${date.getFullYear()}{{ string_2 }}{{ number_2 }}`).getFinalExpression(),
         firstName:'',
         lastName:'',
@@ -19,8 +20,9 @@ export default function CreateUser({type}) {
         imgUrl:'',
         email:'',
         password:'12345',
-        status:''
-    }
+        status:'',
+        role:type
+    })
     const userSchema = yup.object().shape({
         id: yup.string(),
         firstName: yup.string().required('First name is required'),
@@ -29,11 +31,16 @@ export default function CreateUser({type}) {
         tickets: yup.string().required('Default value should be present'),
         imgUrl:yup.string(),
         password:yup.string().required('A default value is required'),
-        status:yup.string().required('default status is required')
+        status:yup.string().required('default status is required'),
+        role:yup.string()
     })
     const handleSubmit = (values)=>{
       if (type==='developer')postDeveloper(values)
       if (type==='agent')postAgents(values)
+      setInitialValues({
+        ...initialValues,
+        id:type==='developer'?new IdGenerator(`dev${date.getFullYear()}{{ string_2 }}{{ number_2 }}`).getFinalExpression():new IdGenerator(`agent${date.getFullYear()}{{ string_2 }}{{ number_2 }}`).getFinalExpression(),
+      })
     }
   return (
     <Container>
@@ -44,109 +51,31 @@ export default function CreateUser({type}) {
           Create a new {type==='developer'?'Developer':'Agent'} account
         </Typography>
         <Formik
-              onSubmit={handleSubmit}
+              enableReinitialize
+              onSubmit={(values,{resetForm})=>{handleSubmit(values);resetForm()}}
               initialValues={initialValues}
               validationSchema={userSchema}
               >
               {
                 ({values, errors, touched, handleChange, handleSubmit, handleBlur})=>(
                   <form onSubmit={handleSubmit}>
-                    <TextField
-                      type='text'
-                      variant='filled'
-                      label='id'
-                      name='id'
-                      disabled
-                      value = {values.id}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.id && !!errors.id}
-                    >
-                    </TextField>
+                    {Object.keys(initialValues).map((data,index)=>(
+                          <TextField
+                            type='text'
+                            variant='filled'
+                            label={data}
+                            name={data}
+                            disabled={data==='id'|| data==='tickets' || data==='role'?true:false}
+                            value = {values[data]}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={!!touched[data] && !!errors[data]}
+                          />
+                        ))}
+                      <Button type='submit'>Create {type} account</Button>
 
-                    <TextField
-                      variant='filled'
-                      label='First Name'
-                      name='firstName'
-                      value = {values.firstName}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.firstName && !!errors.firstName}
-                    >
-                    </TextField>
-
-                    <TextField
-                      variant='filled'
-                      label='Last Name'
-                      name='lastName'
-                      value = {values.lastName}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.lastName && !!errors.lastName}
-                    >
-                    </TextField>
-
-                    <TextField
-                      variant='filled'
-                      label='Email'
-                      name='email'
-                      value = {values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.email && !!errors.email}
-                    >
-                    </TextField>
-
-                    <TextField
-                      variant='filled'
-                      label='Tickets'
-                      name='tickets'
-                      value = {values.tickets}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.tickets && !!errors.tickets}
-                    >
-                    </TextField>
-                    
-                    <TextField 
-                      variant='filled'
-                      label='Image Source'
-                      name='imgUrl'
-                      value = {values.imgUrl}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.imgUrl && !!errors.imgUrl}
-                      helperText={!!touched.imgUrl && errors.imgUrl}
-                    >
-                    </TextField>
-
-                    <TextField 
-                      variant='filled'
-                      label='password'
-                      name='password'
-                      value = {values.password}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.password && !!errors.password}
-                      helperText={!!touched.password && errors.password}
-                    >
-                    </TextField>
-                    <TextField 
-                      variant='filled'
-                      label='status'
-                      name='status'
-                      value = {values.status}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={!!touched.status && !!errors.status}
-                      helperText={!!touched.status && errors.status}
-                    >
-                    </TextField>
-
-                    <Button type='submit' onClick={()=>console.log('sub')}>Create user</Button>
-                  </form>
-                )
-              }
+                      </form>
+                      )}
               </Formik>
             </ComponentContainer>
     </Container>
