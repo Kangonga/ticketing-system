19fb240d5026efb3efaf25c24b8271754f8c111a
Kangonga <guchu413@gmail.com>
2023-04-11 14:08:31 +0300

    feat: check for user login status to protect the agent routes

---
+++ src/authRoutes/RequireAgentAuth.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Route, useNavigate } from 'react-router-dom'
+
+export default function RequireAgentAuth() {
+    const navigate = useNavigate()
+    const userLoggedIn = useSelector((state)=>state.auth.isLoggedIn)
+    useEffect(()=>{
+        if(!userLoggedIn)navigate('/')
+    },[])
+  return (
+    <Route path="/agent">
+        {/* <Route index element={<List />} />
+        <Route path="login" element={<Login user={'agent'}/>} />
+        <Route path=":agentId" element={<ListItem />} />
+        <Route path="new" element={<NewItem />} />
+        <Route path="me" element={<NewItem />} />
+        <Route path="login" element={<Login />} /> */}
+    </Route>
+   )
+}
