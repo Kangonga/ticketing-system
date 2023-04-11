4e2c27404f366d80dd5001c3d9319848bf51b33f
Kangonga <guchu413@gmail.com>
2023-04-11 13:48:32 +0300

    feat: handle login functionality using rtk

--- src/shared/pages/login/Login.jsx
+++ src/shared/pages/login/Login.jsx
@@ -2,14 +2,17 @@ import * as yup from 'yup'
 import { Container, SmallText } from './styles'
 import { Formik } from 'formik'
 import { useContext, useState } from 'react'
-import {  TextField , InputAdornment, IconButton, Typography, Button } from '@mui/material'
+import { TextField , InputAdornment, IconButton, Typography, Button } from '@mui/material'
 import { Visibility, VisibilityOff } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../../../context/UserContext'
+import { fetchUser } from '../../../features/auth/authSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function AdminLogin() {
+  const dispatch = useDispatch()
+  const auth = useSelector((state)=>state.auth)
   const [showPassword,setShowPassword] = useState(false)
-  const {user,setUser} = useContext(UserContext)
   const navigate = useNavigate()
   const [initialData, setInitialData] = useState({
     email:"",
@@ -19,20 +22,12 @@ export default function AdminLogin() {
     email: yup.string().email().required('please input your email'),
     password: yup.string().required('please input your password')
   })
-  const handleSubmit = (data)=>{
-    const updated = {...user, name:'admin', role:'admin'}
-    localStorage.setItem("user",JSON.stringify(updated)) 
-    navigate('/admin')
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    dispatch(fetchUser()).then(()=>navigate('/admin'))
   }
   return (
     <Container>
-      <Formik
-         validationSchema={schema}
-        onSubmit={(values, {resetForm})=>{handleSubmit(values);resetForm()}}
-        initialValues={initialData}
-      >
-        {
-        ({ errors, touched, handleBlur, handleSubmit})=>(
             <form onSubmit={handleSubmit}>
                 <TextField 
                       variant='outlined'
@@ -40,9 +35,6 @@ export default function AdminLogin() {
                       name='email'
                       value = {initialData.email}
                       onChange={(e)=>setInitialData({...initialData, email:`${e.target.value}`})}
-                      onBlur={handleBlur}
-                      error={!!touched.email && !!errors.email}
-                      helperText={!!touched.email && errors.email}
                     >
                     </TextField>
 
@@ -53,9 +45,6 @@ export default function AdminLogin() {
                       name='password'
                       value = {initialData.password}
                       onChange={(e)=>setInitialData({...initialData, password:`${e.target.value}`})}
-                      onBlur={handleBlur}
-                      error={!!touched.password && !!errors.password}
-                      helperText={!!touched && errors.password}
                       type={showPassword?'text':'password'}
                       InputProps={{
                         endAdornment: <InputAdornment position="end">
@@ -68,8 +57,6 @@ export default function AdminLogin() {
                     </TextField>
                     <Button sx={{ width:'max-content', padding:'0.5rem 1rem',backgroundColor:'coral',margin:'auto'}} type='submit'>Submit</Button>
             </form>
-        )}
-      </Formik>
       <SmallText>
         <Typography variant='p' fontSize='.9rem'>Forgot Password? Request  a new password</Typography>
       </SmallText>
