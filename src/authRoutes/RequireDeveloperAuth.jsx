937f762f6cda3255a7a3b79a40093aafaa3e4d80
Kangonga <guchu413@gmail.com>
2023-04-11 16:10:20 +0300

    fix: Get correct state object. Uncomment default route

--- src/authRoutes/RequireDeveloperAuth.jsx
+++ src/authRoutes/RequireDeveloperAuth.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { Route, useNavigate } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import DeveloperDashboard from '../features/developers/pages/dashboard/DeveloperDashboard'
 
 export default function RequireDeveloperAuth() {
     const navigate = useNavigate()
-    const userLoggedIn = useSelector(state=>state.auth.userLoggedIn)
+    const userLoggedIn = useSelector(state=>state.auth.isLoggedIn)
     const userRole = useSelector((state)=>state.auth.userInfo.role)
     useEffect(()=>{
         if(!userLoggedIn || userRole!=='developer')navigate('/')
+        console.log(userLoggedIn, userRole)
     },[])
   return (
-    <Route path="/dev">
-        {/* <Route index element={<List />} />
-        <Route path="login" element={<Login user={'developer'}/>} />
+    <Routes>
+        <Route index element={<DeveloperDashboard />} />
+        {/* <Route path="login" element={<Login user={'developer'}/>} />
         <Route path=":devId" element={<ListItem />} />
         <Route path="new" element={<NewItem />} />
         <Route path="me" element={<NewItem />} />
         <Route path="login" element={<Login />} /> */}
-    </Route>
+    </Routes>
     )
 }
