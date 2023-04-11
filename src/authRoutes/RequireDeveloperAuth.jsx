0045dd8a5951df8c08dd1a89be02e4abb5656823
Kangonga <guchu413@gmail.com>
2023-04-11 14:09:26 +0300

    feat: check if the user is logged in to protect the developer feature routes

---
+++ src/authRoutes/RequireDeveloperAuth.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function RequireDeveloperAuth() {
+    const navigate = useNavigate()
+    const userLoggedIn = useSelector(state=>state.auth.userLoggedIn)
+    useEffect(()=>{
+        if(!userLoggedIn)navigate('/')
+    },[])
+  return (
+    <Route path="/dev">
+        {/* <Route index element={<List />} />
+        <Route path="login" element={<Login user={'developer'}/>} />
+        <Route path=":devId" element={<ListItem />} />
+        <Route path="new" element={<NewItem />} />
+        <Route path="me" element={<NewItem />} />
+        <Route path="login" element={<Login />} /> */}
+    </Route>
+    )
+}
