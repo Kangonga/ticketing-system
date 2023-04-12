e1e2acfcb587aa1be72b61057791c7f42757bf4e
Kangonga <guchu413@gmail.com>
2023-04-12 12:36:28 +0300

    feat: add assign/ unassign functionality to the developer role on the     data table and display create tickets button conditionally based on user role

--- src/features/tickets/datagrid/DataGrid.jsx
+++ src/features/tickets/datagrid/DataGrid.jsx
@@ -3,22 +3,44 @@ import { ticketColumns } from './data/columns';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import {  useNavigate } from 'react-router-dom';
 import { Actions, ComponentContainer } from './styles';
-import { fetchTickets } from '../../../data/fetchData';
+import { fetchTicket, fetchTickets } from '../../../data/fetchData';
 import { useQuery } from '@tanstack/react-query';
 
 import { Container } from '../../../shared/styles/styles';
 import Sidebar from '../../../shared/components/sidebar/Sidebar';
 import Topbar from '../../../shared/components/topbar/Topbar';
+import { useSelector } from 'react-redux';
+import { patchTicket } from '../../../data/patchData';
+import { useState } from 'react';
 
 export default function TicketsDataTable() {
   const navigate = useNavigate()
+  const loggedInUser = useSelector((state)=>state.auth.userInfo)
+  const [isTicketMutated, setIsTicketMutated] = useState(false)
 
-  const { data, isLoading, isError, error } = useQuery(["fetchTickets"], fetchTickets, {networkMode:'offlineFirst'})
+  const { data, isLoading, isError, error } = useQuery(["fetchTickets", isTicketMutated], fetchTickets, {networkMode:'offlineFirst'})
   if(isLoading)return "Loading";
   if(isError) console.log(error)
+  
   const handleView = ( e, params )=>{
     e.preventDefault()
     navigate(`./${params.row.id}`)
+  }
+
+  const handleAssign = async( e, params )=>{
+    e.preventDefault()
+    let ticket = await fetchTicket(params.row.id)
+    let updatedTicket = await {...ticket, developer:loggedInUser}
+    patchTicket(updatedTicket,params.row.id).then((res)=>console.log('response',res))
+    setIsTicketMutated(!isTicketMutated)
+  }
+
+  const handleUnAssign = async( e, params )=>{
+    e.preventDefault()
+    let ticket = await fetchTicket(params.row.id)
+    let updatedTicket = await {...ticket, developer:{}}
+    patchTicket(updatedTicket,params.row.id).then((res)=>console.log('response',res))
+    setIsTicketMutated(!isTicketMutated)
   }
 
   let dataColumns = ticketColumns
@@ -26,7 +48,7 @@ export default function TicketsDataTable() {
   const actions = [{
     field:"action",
     headerName:"Action",
-    width:200,
+    width:100,
     renderCell: (params)=> {
       return (
         <Actions>
@@ -34,12 +56,31 @@ export default function TicketsDataTable() {
         </Actions>
       )
     }
-  }]
+  },
+  {
+    field:"assigned",
+    headerName:"",
+    width:200,
+    renderCell: (params)=> {
+      return (
+        <Actions>
+            <div onClick={e=>handleAssign(e,params)} className="view">
+                {!params.row.developer.id?"Self assign":"assigned"}
+            </div>
+
+            <div onClick={e=>handleUnAssign(e,params)} className="view" style={{display:params.row.developer.id?"inline":"none"}}>
+                {params.row.developer.id?"unassign":""}
+            </div>
+        </Actions>
+      )
+    }
+},
+]
   return (
     <Container>
       <Sidebar></Sidebar>
     <ComponentContainer>
-      <Topbar></Topbar>
+      <Topbar />
         <Typography fontSize='1.2rem' color='gray' textAlign='center' paddingTop='1rem'> Tickets</Typography>
         <DataGrid
             rows={data}
@@ -54,7 +95,11 @@ export default function TicketsDataTable() {
             toolbar: GridToolbar,
             }}
         />
-        <Button onClick={()=>navigate('new')} sx={{width:'max-content',color:'black', padding:'.5rem 1rem', margin:'auto', backgroundColor:'coral'}}>Create new ticket</Button>
+        <Button onClick={()=>navigate('new')}
+         sx={{
+          width:'max-content',color:'black', padding:'.5rem 1rem', margin:'auto', backgroundColor:'coral',
+          display:loggedInUser.role==="admin"||loggedInUser.role==="agent"?"block":"none"
+          }}>Create new ticket</Button>
     </ComponentContainer>
     </Container>
   );
