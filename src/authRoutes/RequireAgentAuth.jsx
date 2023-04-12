2b5cf7b0cb1abe0be9687c0366382c166e4d5a30
Kangonga <guchu413@gmail.com>
2023-04-12 15:20:09 +0300

    feat: add agent routes as children of the parent agent protected route

--- src/authRoutes/RequireAgentAuth.jsx
+++ src/authRoutes/RequireAgentAuth.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { Route, useNavigate } from 'react-router-dom'
+import { Route,Routes, useNavigate } from 'react-router-dom'
+import AgentDashboard from '../features/agents/pages/agentDashboard/AgentDashboard'
+import TicketsDataTable from '../features/tickets/datagrid/DataGrid'
+import TicketChat from '../shared/pages/ticketChat/TicketChat'
+import Profile from '../shared/pages/profilePage/Profile'
 
 export default function RequireAgentAuth() {
     const navigate = useNavigate()
@@ -12,13 +16,16 @@ export default function RequireAgentAuth() {
         if(!userLoggedIn || userRole!=='agent')navigate('/')
     },[])
   return (
-    <Route path="/agent">
-        {/* <Route index element={<List />} />
-        <Route path="login" element={<Login user={'agent'}/>} />
-        <Route path=":agentId" element={<ListItem />} />
-        <Route path="new" element={<NewItem />} />
-        <Route path="me" element={<NewItem />} />
-        <Route path="login" element={<Login />} /> */}
-    </Route>
+    <>
+        <Routes >
+            <Route index element={<AgentDashboard />} />
+            <Route path="tickets">
+                <Route index element={<TicketsDataTable />} />
+                <Route path=':ticketId' element={<TicketChat />} />
+            </Route>
+            <Route path='profile' element={<Profile />} />
+        </Routes>
+    </>
+
    )
 }
