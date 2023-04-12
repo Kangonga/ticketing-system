76e87acf946afb6674fb19317fadbecd08a53e73
Kangonga <guchu413@gmail.com>
2023-04-12 12:34:10 +0300

    feat: add charts to developer dashboard

--- src/features/developers/pages/dashboard/DeveloperDashboard.jsx
+++ src/features/developers/pages/dashboard/DeveloperDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container } from '../../../../shared/styles/styles'
 import Sidebar from '../../../../shared/components/sidebar/Sidebar'
-import { ComponentContainer } from './styles'
+import { AreaChartContainer, ChartContainer, ComponentContainer, ProgressBarContainer } from './styles'
 import { Widgets } from '../../../admin/pages/adminDashboard/styles'
 import Widget from '../../../admin/components/widgets/widget'
 import { useSelector } from 'react-redux'
 import { Typography } from '@mui/material'
 import Topbar from '../../../../shared/components/topbar/Topbar'
+import AreaChart from '../../../../shared/components/charts/areaChart/AreaChart'
+import ClosedTicketsCard from '../../../admin/components/closedTicketsCard/ClosedTicketsCard'
 
 export default function DeveloperDashboard() {
     const user = useSelector((state)=>state.auth.userInfo)
@@ -21,6 +23,19 @@ export default function DeveloperDashboard() {
                 <Widget type='devClosedTickets' />
                 <Widget type='devTotalOpenTickets' />
             </Widgets>
+            <ChartContainer>
+                <ProgressBarContainer>
+                    <ClosedTicketsCard />
+                </ProgressBarContainer>
+
+                <AreaChartContainer>
+                    Tickets closed over time
+                    <AreaChart />
+                </AreaChartContainer>
+
+
+
+            </ChartContainer>
         </ComponentContainer>
     </Container>
 )
--- src/features/developers/pages/dashboard/styles.jsx
+++ src/features/developers/pages/dashboard/styles.jsx
@@ -1,3 +1,4 @@
+import { Margin } from "@mui/icons-material";
 import { styled } from "@mui/material";
 
 export const ComponentContainer = styled('div')(({ theme })=>({
@@ -7,4 +8,32 @@ export const ComponentContainer = styled('div')(({ theme })=>({
     backgroundColor:'#fcfcfc',
     height:'max-content',
     padding:'0 1rem'
-}))    
+}))
+
+export const ChartContainer = styled('div')(({ theme })=>({
+    display:'flex',
+    gap:'2rem',
+    justifyContent:'space-around',
+    [theme.breakpoints.down('md')]:{
+        flexDirection:'column'
+    }
+}))
+
+export const AreaChartContainer = styled('div')(({ theme })=>({
+    // height: '300px',
+    minWidth:'25rem',
+    // maxWidth:'30rem',
+    // padding:'1rem'
+    height: '280px',
+    borderRadius: ' 10px',
+    boxShadow: ' 2px 2px 5px rgba(0, 0, 0, 0.3)',
+    padding: ' 20px',
+    gap:'.75rem',
+    // margin:'auto'
+}))
+export const ProgressBarContainer = styled('div')(({ theme })=>({
+    height: '250px',
+    minWidth:'25rem',
+    maxWidth:'30rem',
+    // margin:'auto'
+}))
