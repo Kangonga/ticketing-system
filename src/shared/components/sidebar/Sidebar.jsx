48b745cffeccec3d168f880bc68406a325dc249e
Kangonga <guchu413@gmail.com>
2023-04-12 09:07:10 +0300

    refactor: Manually create the sidebar icons, add role specific routes when clicked and render the icons conditionally based on user role

--- src/shared/components/sidebar/Sidebar.jsx
+++ src/shared/components/sidebar/Sidebar.jsx
@@ -4,13 +4,15 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { icons } from './data'
 import { Drawer, DrawerHeader } from './styles'
 import MenuIcon from '@mui/icons-material/Menu';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { authActions } from '../../../features/auth/authSlice'
+import { AnalyticsOutlined, BugReportOutlined, EngineeringOutlined, HomeOutlined, LiveHelpOutlined, LogoutOutlined, PersonOutlined, SupportAgentOutlined } from '@mui/icons-material'
 
 export default function Sidebar() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   let location = useLocation()
+  const userRole = useSelector((state)=>state.auth.userInfo.role)
   location = location.pathname.split('/')[1]
   const [openDrawer, setOpenDrawer] = useState(false)
   const handleLogout = ()=>{
@@ -36,14 +38,12 @@ export default function Sidebar() {
               </IconButton>
           </DrawerHeader>
           <Divider />
-          
-          {Object.keys(icons).map((iconGroup) => (
-          <List key={iconGroup}>
-            {icons[iconGroup].map((icon,index)=>(
-              <ListItem disablePadding sx={{ display: 'block' }} key={index}
-               onClick={()=>{icon.name==='Log Out'?handleLogout():navigate(`${icon.route}`)}}
-               >
-              <ListItemButton
+
+        <List >
+            <ListItem disablePadding sx={{ display: 'block' }}
+              onClick={()=>navigate(`/${userRole}`)}
+            >
+            <ListItemButton
                 sx={{
                   minHeight: 48,
                   justifyContent: openDrawer ? 'initial' : 'center',
@@ -57,23 +57,185 @@ export default function Sidebar() {
                     justifyContent: 'center',
                   }}
                 >
-                    {icon.icon}
+                    <HomeOutlined />
                 </ListItemIcon>
-                <ListItemText primary={icon.name} sx={{ opacity: openDrawer ? 1 : 0 }} />
+                <ListItemText primary='Dashboard' sx={{ opacity: openDrawer ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
-            ))}
-            <Divider />
-
-            {/* <IconButton onClick={()=>setMode(mode==='light'?'dark':'light')}>
-                {mode==="dark"?
-                <LightModeOutlined />:
-                <DarkModeOutlined />
-                }
-              </IconButton> */}
         </List>
-        ))
-        }
+        <Divider />
+        <List sx={{display:userRole!=='admin'?'none':'block'}}>
+            <ListItem disablePadding sx={{ display: 'block' }}
+               onClick={()=>navigate(`/${userRole}/devs`)} 
+            >
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <EngineeringOutlined />
+                </ListItemIcon>
+                <ListItemText primary='Developers' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding sx={{ display: 'block' }}
+               onClick={()=>navigate(`/${userRole}/agents`)} 
+            >
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <SupportAgentOutlined />
+                </ListItemIcon>
+                <ListItemText primary='Tickets' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+        </List>
+
+        <List>
+        <ListItem disablePadding sx={{ display: 'block' }}
+          onClick={()=>navigate(`/${userRole}/tickets`)}
+        >
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <BugReportOutlined />
+                </ListItemIcon>
+                <ListItemText primary='Tickets' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+        </List>
+
+          <Divider />
+
+        <List sx={{display:userRole!=='admin'?'none':'block'}}>
+            <ListItem disablePadding sx={{ display: 'block' }}
+              onClick={()=>navigate(`/${userRole}/Statistics`)}
+            >
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <AnalyticsOutlined />
+                </ListItemIcon>
+                <ListItemText primary='Statistics' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding sx={{ display: 'block' }}
+              onClick={()=>navigate(`/${userRole}/faq`)}
+            >
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <LiveHelpOutlined />
+                </ListItemIcon>
+                <ListItemText primary='FAQs' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+        </List>
+
+        <List>
+            <ListItem disablePadding sx={{ display: 'block' }}
+              onClick={()=>navigate(`/${userRole}/profile`)}
+              >
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <PersonOutlined />
+                </ListItemIcon>
+                <ListItemText primary='Profile' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+        </List>
+          
+        <List>
+            <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: openDrawer ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: openDrawer ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                    <LogoutOutlined />
+                </ListItemIcon>
+                <ListItemText primary='Log Out' sx={{ opacity: openDrawer ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+        </List>
+
 
       </Drawer>
     </Box>
