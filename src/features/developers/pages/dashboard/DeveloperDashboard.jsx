d687147df4f87d5e3484756bb6299df2a85fa4da
Kangonga <guchu413@gmail.com>
2023-04-12 09:08:39 +0300

    feat: create the developer dashboard

--- src/features/developers/pages/dashboard/DeveloperDashboard.jsx
+++ src/features/developers/pages/dashboard/DeveloperDashboard.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Container } from '../../../../shared/styles/styles'
+import Sidebar from '../../../../shared/components/sidebar/Sidebar'
+import { ComponentContainer } from './styles'
+import { Widgets } from '../../../admin/pages/adminDashboard/styles'
+import Widget from '../../../admin/components/widgets/widget'
+import { useSelector } from 'react-redux'
+import { Typography } from '@mui/material'
+import Topbar from '../../../../shared/components/topbar/Topbar'
 
 export default function DeveloperDashboard() {
+    const user = useSelector((state)=>state.auth.userInfo)
   return (
-    <div>DeveloperDashboard</div>
-  )
+    <Container>
+        <Sidebar />
+        <ComponentContainer>
+            <Topbar />
+            <Typography>Hello, {user?.firstName} {user?.lastName}</Typography>
+            <Widgets>
+                <Widget type='devOpenTickets' />
+                <Widget type='devClosedTickets' />
+                <Widget type='devTotalOpenTickets' />
+            </Widgets>
+        </ComponentContainer>
+    </Container>
+)
 }
---
+++ src/features/developers/pages/dashboard/styles.jsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+
+export const ComponentContainer = styled('div')(({ theme })=>({
+    flex:2,
+    display: 'flex',
+    flexDirection:'column',
+    backgroundColor:'#fcfcfc',
+    height:'max-content',
+    padding:'0 1rem'
+}))    
