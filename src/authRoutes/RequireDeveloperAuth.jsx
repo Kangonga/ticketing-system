1d4a3f610925f95afda8acc7842406b0d6027254
Kangonga <guchu413@gmail.com>
2023-04-12 09:09:56 +0300

    feat: add developer routes as children to the parent protected route

--- src/authRoutes/RequireDeveloperAuth.jsx
+++ src/authRoutes/RequireDeveloperAuth.jsx
@@ -3,6 +3,9 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import DeveloperDashboard from '../features/developers/pages/dashboard/DeveloperDashboard'
+import TicketsDataTable from '../features/tickets/datagrid/DataGrid'
+import TicketChat from '../shared/pages/ticketChat/TicketChat'
+import Profile from '../shared/pages/profilePage/Profile'
 
 export default function RequireDeveloperAuth() {
     const navigate = useNavigate()
@@ -15,11 +18,11 @@ export default function RequireDeveloperAuth() {
   return (
     <Routes>
         <Route index element={<DeveloperDashboard />} />
-        {/* <Route path="login" element={<Login user={'developer'}/>} />
-        <Route path=":devId" element={<ListItem />} />
-        <Route path="new" element={<NewItem />} />
-        <Route path="me" element={<NewItem />} />
-        <Route path="login" element={<Login />} /> */}
+        <Route path="tickets">
+            <Route index element={<TicketsDataTable/>} />
+            <Route path=":ticketId" element={<TicketChat />} />
+        </Route>
+        <Route path="profile" element={<Profile user={'developer'}/>} />
     </Routes>
     )
 }
